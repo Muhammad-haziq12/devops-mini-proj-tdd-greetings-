880dba8b473cba6bf3538b36d3cc95c6382e1fa8
Quisquater Martin <martin.quisquater@student.vinci.be>
2023-11-08 11:22:14 +0100

    Updated the greet function to handle cases when the name is null, undefined, or an empty string.

--- app.test.js
+++ app.test.js
@@ -2,5 +2,17 @@
 const greet = require('./app')
 
 test('greet returns a greeting for the person', () => {
-  expect(greet('Foo')).toBe('Hello, Foo')
+  expect(greet('Bob')).toBe('Hello, Bob')
+})
+
+test('greet returns a greeting for no name', () => {
+  expect(greet()).toBe('Hello, my friend')
+})
+
+test('greet returns a greeting for null', () => {
+  expect(greet(null)).toBe('Hello, my friend')
+})
+
+test('greet returns a greeting for empty string', () => {
+  expect(greet('')).toBe('Hello, my friend')
 })
